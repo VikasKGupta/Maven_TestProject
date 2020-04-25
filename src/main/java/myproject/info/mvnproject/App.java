173bc8702c0dfb2c4d11b9f071332c52954b3f29
Vikas K Gupta <40080381+VikasKGupta@users.noreply.github.com>
2020-04-25 19:06:51 +0530

    Add files via upload

---
+++ src/main/java/myproject/info/mvnproject/App.java
@@ -0,0 +1,13 @@
+package myproject.info.mvnproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
